5f738a9fbae83bb1519f430875a8a1a9a2f5acad
twardoch <adam@twardoch.com>
2021-06-26 00:53:59 +0200

    up

--- tools/md_build.py
+++ tools/md_build.py
@@ -5,73 +5,169 @@
 
 __version__ = '0.0.1'
 
-import fontTools.ttLib
-from sympy import Order
-import fontTools.unicodedata as ucd
-from pathlib import Path, PurePath
-
-from collections import OrderedDict
+import io
 import random
+import re
+from collections import OrderedDict
+from pathlib import Path
+
+import cairosvg
+import fontTools.ttLib
+import fontTools.unicodedata as ucd
+import vharfbuzz
+from PIL import Image
 from yaplon import oyaml, ojson
-import json
 
-class GetGoDocs(object):
 
-    def __init__(self):
-        self.paths = OrderedDict()
-        self.redo_woff = False
-        self.redo_yaml = False
-        self.root_folder = Path(Path(__file__).parent, '..').resolve()
-        self.font_folder = Path(self.root_folder, 'getgo-fonts').resolve()
-        self.download_base = 'https://raw.githubusercontent.com/fontlabcom/getgo-fonts/main/getgo-fonts'
-        self.github_base = 'https://github.com/fontlabcom/getgo-fonts/blob/main/getgo-fonts'
-        self.gitdownload_base = 'https://downgit.github.io/#/home?url=https://github.com/fontlabcom/getgo-fonts/blob/main/getgo-fonts'
-        self.data = OrderedDict()
+class GetGoFont(object):
 
-    def make_paths(self):
-        md_folder = Path(self.root_folder, 'srcdocs').resolve()
-        docs_folder = Path(self.root_folder, 'docs').resolve()
-        woff_folder = Path(self.root_folder, 'docs', 'fonts').resolve()
+    def __init__(self, path, folders, url_bases, redo):
+        self.path = path
+        self.folders = folders
+        self.url_bases = url_bases
+        self.redo = redo
+        self.vfj_path = Path(
+            self.path.parent, str(self.path.stem) + '.vfj'
+        ).resolve()
+        self.ttf_path = Path(
+            self.path.parent, str(self.path.stem) + '.ttf'
+        ).resolve()
+        self.md_path = Path(
+            self.path.parent, str(self.path.stem) + '.md'
+        ).resolve()
+        self.svg_path = Path(
+            self.path.parent, str(self.path.stem) + '.svg'
+        ).resolve()
+        self.png_path = Path(
+            self.path.parent, str(self.path.stem) + '.png'
+        ).resolve()
+        self.woff_path = Path(
+            self.folders['woff'], self.path.stem + '.woff2'
+        ).resolve()
+        self.yaml_path = Path(
+            self.path.parent, str(self.path.stem) + '.yaml'
+        ).resolve()
+        self.font = fontTools.ttLib.TTFont(self.path)
+        self.name_table = self.font['name']
+        self.family = self.get_family_name()
+        self.full_name = self.get_full_name()
+        self.copyright = self.get_copyright()
+        self.license = self.get_license()
+        self.unicodes = sorted(list(self.font.getBestCmap().keys()))
+        self.scripts = OrderedDict()
+        self.script_names = OrderedDict()
+        self.metadata = OrderedDict()
+        self.process()
 
-        for font_path in self.font_folder.glob('**/*.?tf'):
-            font_path = font_path.resolve()
-            self.paths[font_path] = {}
-            frec = self.paths[font_path]
-            frec['md'] = Path(
-                font_path.parent, str(font_path.stem) + '.md'
-                ).resolve()
-            frec['yaml'] = Path(
-                font_path.parent, str(font_path.stem) + '.yaml'
-                ).resolve()
-            frec['vfj'] = Path(
-                font_path.parent, str(font_path.stem) + '.vfj'
-                ).resolve()
-            frec['ttf'] = Path(
-                font_path.parent, str(font_path.stem) + '.ttf'
-            ).resolve()
-            frec['woff'] = Path(
-                woff_folder, font_path.stem + '.woff2'
-                ).resolve()
 
-    def make_woff(self, font, path):
-        if self.redo_woff or not path.is_file():
-            font.flavor = 'woff2'
-            font.save(path)
+    def get_download_url(self, path):
+        url = str(path).replace(
+            str(self.folders['root']),
+            str(self.url_bases['download'])
+        )
+        return url
 
-    def get_sample_text(self, font, words=5, chars=6):
-        unicodes = sorted(list(font.getBestCmap().keys()))
+    def get_family_name(self):
+        family = self.name_table.getName(16, 3, 1)
+        if not family:
+            family = self.name_table.getName(1, 3, 1)
+        return family.toUnicode() if family else ''
+
+    def get_full_name(self):
+        full_name = self.name_table.getName(4, 3, 1)
+        return full_name.toUnicode() if full_name else ''
+
+    def get_copyright(self):
+        copyright = self.name_table.getName(0, 3, 1)
+        return copyright.toUnicode() if copyright else ''
+
+    def get_license(self):
+        license = self.name_table.getName(13, 3, 1)
+        return license.toUnicode() if license else ''
+
+    def build_scripts(self):
+        scripts = OrderedDict()
+        for u in self.unicodes:
+            if ucd.category(chr(u))[0] not in ('N', 'C'):
+                script = ucd.script(chr(u))
+                scripts[script] = scripts.get(script, 0) + 1
+        scripts = OrderedDict(sorted(
+            scripts.items(), key=lambda t: t[1], reverse=True
+        ))
+        for k in ('Zyyy', 'Zinh', 'Zzzz'):
+            if k in scripts.keys():
+                del scripts[k]
+        if len(self.unicodes) < 10000:
+            for k in list(scripts.keys()):
+                if scripts[k] / len(self.unicodes) < 0.11:
+                    del scripts[k]
+        self.scripts = list(scripts.keys()) if len(scripts.keys()) else ['Zsym']
+        self.script_names = [
+            ucd.script_name(script)
+            for script in list(scripts.keys())
+        ]
+
+    def save_woff(self):
+        if self.redo['woff'] or not self.woff_path.is_file():
+            self.font.flavor = 'woff2'
+            self.font.save(self.woff_path)
+
+    def get_font_css(self):
+        url = str(self.woff_path).replace(
+            str(self.folders['docs']),
+            str('..')
+        )
+        return f"""
+
+@font-face {{
+  font-family: '{self.family}';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url({url}) format('woff2');
+}}"""
+
+    def build_metadata(self):
+        metadata = OrderedDict()
+        if not self.redo['yaml'] and self.yaml_path.is_file():
+            with open(self.yaml_path, 'r', encoding='utf-8') as f:
+                metadata = oyaml.read_yaml(f)
+        metadata['full_name'] = metadata.get(
+            'full_name', self.get_full_name())
+        metadata['family_name'] = metadata.get(
+            'family_name', self.get_family_name())
+        metadata['copyright'] = metadata.get(
+            'copyright', self.get_copyright())
+        metadata['license'] = metadata.get('license', self.get_license())
+        metadata['description'] = metadata.get('description', 'Font')
+        metadata['scripts'] = metadata.get('scripts', self.scripts)
+        if self.redo['sample_text']:
+            metadata['sample_text'] = self.get_sample_text()
+        else:
+            metadata['sample_text'] = metadata.get(
+                'sample_text', self.get_sample_text()
+            )
+        with open(self.yaml_path, 'w', encoding='utf-8') as f:
+            oyaml.yaml_dump(metadata, f)
+        self.metadata = metadata
+
+    def get_sample_text(self, words=5, chars=6):
         cats = OrderedDict()
-        for u in unicodes:
+        for u in self.unicodes:
             cat = ucd.category(chr(u))[0]
-            if cat in 'LNS':
+            if cat in 'LNS' and ucd.script(chr(u)) == self.scripts[0]:
                 cats[cat] = cats.get(cat, []) + [u]
-        unicodes = sorted(cats.items(), key=lambda t: len(t[1]), reverse=True)[0][1]
+        try:
+            unicodes = sorted(cats.items(), key=lambda t: len(
+                t[1]), reverse=True)[0][1]
+        except:
+            unicodes = self.unicodes
         lensample = words*chars
         if lensample > len(unicodes):
             lensample = len(unicodes)
         sample = "".join([
             chr(u) for u in random.sample(unicodes, lensample)
-            ])
+        ])
         words = [
             sample[0+i:chars+i]
             for i in range(0, len(sample), chars)
@@ -84,93 +180,88 @@ class GetGoDocs(object):
                 newwords.append(word.upper())
         return " ".join(newwords)
 
-    def get_family_name(self, font):
-        name = font['name']
-        family = name.getName(16, 3, 1)
-        if not family:
-            family = name.getName(1, 3, 1)
-        return family.toUnicode() if family else ''
+    def render_sample_text(self, text=None):
+        if not text:
+            text = self.metadata['sample_text']
+        v = vharfbuzz.Vharfbuzz(self.path)
+        buf = v.shape(text, {'script': self.scripts[0]})
+        svg = v.buf_to_svg(buf)
+        svg = re.sub(
+            r"""<svg xmlns="http://www\.w3\.org/2000/svg" viewBox="0 0 (.*?) (.*?)" transform="matrix\(1 0 0 -1 0 0\)">""",
+            r"""<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 \g<1> \g<2>" transform="translate(0, \g<2>) scale(1, -1)">""",
+            svg
+        )
+        png = io.BytesIO()
+        with open(self.svg_path, 'w') as f:
+            f.write(svg)
+        cairosvg.svg2png(
+            bytestring=svg,
+            write_to=png,
+            output_height=56
+        )
+        im = Image.open(png)
+        im = im.crop((0, 0, 600, 56))
+        im.save(self.png_path, 'PNG')
 
-    def get_full_name(self, font):
-        name = font['name']
-        full_name = name.getName(4, 3, 1)
-        return full_name.toUnicode() if full_name else ''
+    def process(self):
+        self.build_scripts()
+        self.build_metadata()
+        self.save_woff()
+        self.render_sample_text()
 
-    def get_copyright(self, font):
-        name = font['name']
-        copyright = name.getName(0, 3, 1)
-        return copyright.toUnicode() if copyright else ''
 
-    def get_license(self, font):
-        name = font['name']
-        license = name.getName(13, 3, 1)
-        return license.toUnicode() if license else ''
+class GetGoDocs(object):
 
-    def get_scripts(self, font):
-        scripts = OrderedDict()
-        unicodes = sorted(list(font.getBestCmap().keys()))
-        for u in unicodes:
-            if ucd.category(chr(u))[0] not in ('N', 'C'):
-                script = ucd.script(chr(u))
-                scripts[script] = scripts.get(script, 0) + 1
-        scripts = OrderedDict(sorted(
-            scripts.items(), key=lambda t: t[1], reverse=True
-        ))
-        for k in ('Zyyy', 'Zinh', 'Zzzz'):
-            if k in scripts.keys():
-                del scripts[k]
-        if len(unicodes) < 10000:
-            for k in list(scripts.keys()):
-                if scripts[k]/len(unicodes) < 0.11:
-                    del scripts[k]
-        script_names = [
-            ucd.script_name(script)
-            for script in list(scripts.keys())
-            ]
-        return script_names
+    def __init__(self):
+        self.paths = OrderedDict()
+        self.redo = {}
+        self.redo['woff'] = False
+        self.redo['yaml'] = False
+        self.redo['sample_text'] = False
+        self.folders = {}
+        self.folders['root'] = Path(Path(__file__).parent, '..').resolve()
+        self.folders['font'] = Path(self.folders['root'], 'getgo-fonts').resolve()
+        self.folders['md'] = Path(self.folders['root'], 'srcdocs').resolve()
+        self.folders['docs'] = Path(self.folders['root'], 'docs').resolve()
+        self.folders['woff'] = Path(self.folders['docs'], 'fonts').resolve()
+        self.folders['css'] = Path(self.folders['docs'], 'css').resolve()
 
-    def process_metadata(self, font, frec):
-        metadata = OrderedDict()
-        if not self.redo_yaml and frec['yaml'].is_file():
-            with open(frec['yaml'], 'r', encoding='utf-8') as f:
-                metadata = oyaml.read_yaml(f)
-        metadata['full_name'] = metadata.get('full_name', self.get_full_name(font))
-        metadata['family_name'] = metadata.get('family_name', self.get_family_name(font))
-        metadata['copyright'] = metadata.get('copyright', self.get_copyright(font))
-        metadata['license'] = metadata.get('license', self.get_license(font))
-        metadata['description'] = metadata.get('description', 'Font')
-        metadata['scripts'] = metadata.get('scripts', self.get_scripts(font))
-        metadata['sample_text'] = metadata.get('sample_text', self.get_sample_text(font))
-        with open(frec['yaml'], 'w', encoding='utf-8') as f:
-            oyaml.yaml_dump(metadata, f)
-        return metadata
+        self.url_bases = {}
 
-    def process_font(self, path, frec):
-        font = fontTools.ttLib.TTFont(path)
-        self.make_woff(font, frec['woff'])
-        metadata = self.process_metadata(font, frec)
-        return metadata
+        self.url_bases['web'] = 'https://raw.githubusercontent.com/fontlabcom/getgo-fonts/main/docs'
+        self.url_bases['download'] = 'https://raw.githubusercontent.com/fontlabcom/getgo-fonts/main'
+        self.url_bases['github'] = 'https://github.com/fontlabcom/getgo-fonts/blob/main/getgo-fonts'
+        self.url_bases['git_download'] = 'https://downgit.github.io/#/home?url=https://github.com/fontlabcom/getgo-fonts/blob/main/getgo-fonts'
+        self.data = OrderedDict()
+        self.font_css = ''
 
-    def get_download_url(self, path):
-        return str(path).replace(
-            str(self.font_folder),
-            str(self.download_base)
-            )
+    def find_fonts(self):
+        for self.path in self.folders['font'].glob('**/*.?tf'):
+            self.path = self.path.resolve()
+            self.paths[self.path] = {}
 
-    def process_fonts(self):
-        for path, frec in self.paths.items():
-            metadata = self.process_font(path, frec)
-            self.data[metadata['full_name']] = OrderedDict()
-            drec = self.data[metadata['full_name']]
-            drec.update(metadata)
-            drec['vfj_url'] = self.get_download_url(frec['vfj'])
-            drec['ttf_url'] = self.get_download_url(frec['ttf'])
-            drec['md_url'] = self.get_download_url(frec['md'])
-        print(json.dumps(self.data, ensure_ascii=False, indent=2))
+    def process(self):
+        for path in self.paths:
+            fo = GetGoFont(path, self.folders, self.url_bases, self.redo)
+
+            self.data[fo.metadata['full_name']] = OrderedDict()
+            drec = self.data[fo.metadata['full_name']]
+            drec.update(fo.metadata)
+            drec['vfj_url'] = fo.get_download_url(fo.vfj_path)
+            drec['ttf_url'] = fo.get_download_url(fo.ttf_path)
+            drec['md_url'] = fo.get_download_url(fo.md_path)
+            drec['svg_url'] = fo.get_download_url(fo.svg_path)
+            drec['png_url'] = fo.get_download_url(fo.png_path)
+            self.font_css += fo.get_font_css()
+
+        with open(Path(self.folders['css'], 'fonts.css'), 'w', encoding='utf-8') as f:
+            f.write(self.font_css)
+        with open(Path(self.folders['root'], 'fonts.json'), 'w', encoding='utf-8') as f:
+            ojson.json_dump(self.data, f)
 
     def make(self):
-        self.make_paths()
-        self.process_fonts()
+        self.find_fonts()
+        self.process()
 
 def main():
     ggd = GetGoDocs()
